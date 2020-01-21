1042cd7cee7a9c71c341736e1b903fddaf54d50d
Khamzat <xamzatseynaro@gmail.com>
2020-01-21 16:34:27 +0300

    add todoListTask component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks = [
+        {title: "HTML", isDone: true, priority: "high"},
+        {title: "CSS", isDone: true, priority: "low"},
+        {title: "JS", isDone: true, priority: "high"},
+        {title: "React", isDone: false, priority: "high"},
+    ];
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader />
-                    <TodoListTasks />
+                    <TodoListTasks tasks={this.tasks}/>
                     <TodoListFooter />
                 </div>
             </div>
