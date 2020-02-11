a2435e5d8c19f9a7c5d418f8d29b44c8e155e207
Khamzat <you@example.comxamzatseynaro@gmail.com>
2020-02-11 15:44:35 +0300

    add error, delete ref

--- src/TodoListHeader.js
+++ src/TodoListHeader.js
@@ -4,35 +4,54 @@ import './App.css';
 class TodoListHeader extends React.Component {
 
     state = {
-        error: true,
+        error: false,
+        title: "",
     };
 
 
     newTaskTitleRef = React.createRef();
 
     onAddTaskButtonClick = () => {
-        let newText = this.newTaskTitleRef.current.value;
-        this.newTaskTitleRef.current.value = "";
-        if(newText === "") {
+        let newTitle = this.state.title;
+        this.setState({title: ""})
+        if (newTitle === "") {
             this.setState({error: true})
         } else {
             this.setState({error: false});
-            this.props.addTask(newText)
+            this.props.addTask(newTitle);
+        }
+    };
+
+    onTitleChanged = (e) => {
+        let newTitle = e.currentTarget.value
+            this.setState({
+                error: false,
+                title: newTitle,
+            })
+    };
+
+    onAddTaskEnterPress = (e) => {
+        if (e.key === "Enter") {
+            this.onAddTaskButtonClick()
         }
     };
 
     render = () => {
+        let classForInput = (this.state.error) ? "error" : "";
         return (
             <div className="todoList-header">
-                    <h3 className="todoList-header__title">What to Learn</h3>
-                    <div className="todoList-newTaskForm">
-                        <input ref={this.newTaskTitleRef}
-                               type="text" placeholder="New task name"
-                               ref={this.newTaskTitleRef}
-                               className="error"
-                        />
-                        <button onClick={this.onAddTaskButtonClick}>Add</button>
-                    </div>
+                <h3 className="todoList-header__title">What to Learn</h3>
+                <div className="todoList-newTaskForm">
+                    <input
+                        value={this.state.title}
+                        onChange={this.onTitleChanged}
+                        type="text"
+                        placeholder="New task name"
+                        className={classForInput}
+                        onKeyPress={this.onAddTaskEnterPress}
+                    />
+                    <button onClick={this.onAddTaskButtonClick}>Add</button>
+                </div>
             </div>
         );
     }
--- src/TodoListTask.js
+++ src/TodoListTask.js
@@ -2,18 +2,22 @@ import React from 'react'
 import './App.css';
 
 class TodoListTask extends React.Component {
+
     onIsDoneChanged = (e) => {
         this.props.changeStatus(e.currentTarget.checked, this.props.task);
-    }
+    };
 
     render = () => {
+        let classForTask = (this.props.task.isDone)
+            ? "todoList-task done"
+            : "todoList-task";
         return (
-                    <div className="todoList-task">
-                        <input type="checkbox"
-                               checked={this.props.task.isDone}
-                        onChange={this.onIsDoneChanged}/>
-                        <span>{this.props.task.title}, priority: {this.props.task.priority}</span>
-                    </div>
+            <div className={classForTask}>
+                <input type="checkbox"
+                       checked={this.props.task.isDone}
+                       onChange={this.onIsDoneChanged}/>
+                <span>{this.props.task.title}, priority: {this.props.task.priority}</span>
+            </div>
         );
     }
 }
