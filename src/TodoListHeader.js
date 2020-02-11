ccd8d4e94ee6b3c8eb0026c59a35615fc77312c3
Khamzat <you@example.comxamzatseynaro@gmail.com>
2020-02-11 13:49:29 +0300

    add hide/show

--- src/TodoListHeader.js
+++ src/TodoListHeader.js
@@ -3,20 +3,34 @@ import './App.css';
 
 class TodoListHeader extends React.Component {
 
-    newTaskTitleRef = React.createRef()
+    state = {
+        error: true,
+    };
+
+
+    newTaskTitleRef = React.createRef();
 
     onAddTaskButtonClick = () => {
         let newText = this.newTaskTitleRef.current.value;
         this.newTaskTitleRef.current.value = "";
-        this.props.addTask(newText)
-    }
+        if(newText === "") {
+            this.setState({error: true})
+        } else {
+            this.setState({error: false});
+            this.props.addTask(newText)
+        }
+    };
 
     render = () => {
         return (
             <div className="todoList-header">
                     <h3 className="todoList-header__title">What to Learn</h3>
                     <div className="todoList-newTaskForm">
-                        <input ref={this.newTaskTitleRef} type="text" placeholder="New task name" ref={this.newTaskTitleRef}/>
+                        <input ref={this.newTaskTitleRef}
+                               type="text" placeholder="New task name"
+                               ref={this.newTaskTitleRef}
+                               className="error"
+                        />
                         <button onClick={this.onAddTaskButtonClick}>Add</button>
                     </div>
             </div>
